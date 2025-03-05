be512a41e59217b34ece50af8fafe02050c5b59b
vanelNw <vanelnwaba@gmail.com>
2025-03-05 10:12:23 +0100

    remove comment

--- server.js
+++ server.js
@@ -84,75 +84,9 @@ const getNearbyPartners = async (location) => {
 
 export const getActiveOrders = () => {return activeOrders};
 
-console.log('getActiveorders : ',getActiveOrders());
-
 // Start server and listener
 server.listen(5000, () => {
   console.log('✅ Serveur en cours d’exécution sur le port 5000');
   setupOrderListener();
 });
 
-
-
-
-
-
-// const express = require('express');
-// const http = require('http');
-// const cors = require('cors');
-// const { setupSocket } = require('./socket');
-// const { db } = require('./firebase');
-// const { sendNotificationsToPartners, sendNotificationToCustomer } = require('./notifications');
-// const { calculateDistance } = require('./utils/distance');
-
-// const app = express();
-// const server = http.createServer(app);
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Setup WebSocket
-// setupSocket(server);
-
-// // Listen for Firestore changes
-// const setupOrderListener = () => {
-//   db.collection('orders').onSnapshot(async (snapshot) => {
-//     for (const change of snapshot.docChanges()) {
-//       if (change.type === 'added') {
-//         const order = change.doc.data();
-//         try {
-//           const partners = await getNearbyPartners(order.restaurantLocation);
-//           await sendNotificationsToPartners(partners, order);
-//         } catch (error) {
-//           console.error('Error processing new order:', error);
-//         }
-//       }
-//       if (change.type === 'modified') {
-//         const newData = change.doc.data();
-//         const oldData = change.oldData;
-//         if (newData.status === 'picked_up' && oldData.status !== 'picked_up') {
-//           try {
-//             await sendNotificationToCustomer(newData.customerId);
-//           } catch (error) {
-//             console.error('Error notifying customer:', error);
-//           }
-//         }
-//       }
-//     }
-//   });
-// };
-
-// // Helper function to get nearby partners
-// const getNearbyPartners = async (location) => {
-//   const partnersRef = db.collection('delivery_partners');
-//   const querySnapshot = await partnersRef.where('isOnline', '==', true).get();
-//   return querySnapshot.docs.map(doc => doc.data())
-//    // .filter(partner => calculateDistance(location, partner.location) <= 10);
-// };
-
-// // Start server and listener
-// server.listen(5000, () => {
-//   console.log('Server running on port 5000');
-//   setupOrderListener();
-// });
